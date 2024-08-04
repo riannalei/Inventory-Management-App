39ad67dd5060143c8c597c77979048ec584c6f8f
riannalei <riannalei@gmail.com>
2024-08-04 00:51:47 -0700

    updated pagejs

--- firebase.js
+++ firebase.js
@@ -1,19 +1,21 @@
-// firebase.js
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Replace with your Firebase configuration
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyD8EBvAKEtOo0CqWpcq1FPFFYHmji33v58",
-    authDomain: "inventory-management-app-4ad3d.firebaseapp.com",
-    projectId: "inventory-management-app-4ad3d",
-    storageBucket: "inventory-management-app-4ad3d.appspot.com",
-    messagingSenderId: "988871786343",
-    appId: "1:988871786343:web:78644fdd25b96798c179c2",
-    measurementId: "G-95Z74FY83E"
-  };
-  
-const app = initializeApp(firebaseConfig);
-const firestore = getFirestore(app);
+  apiKey: "AIzaSyD8EBvAKEtOo0CqWpcq1FPFFYHmji33v58",
+  authDomain: "inventory-management-app-4ad3d.firebaseapp.com",
+  projectId: "inventory-management-app-4ad3d",
+  storageBucket: "inventory-management-app-4ad3d.appspot.com",
+  messagingSenderId: "988871786343",
+  appId: "1:988871786343:web:7e6964f1ad4ed0dec179c2",
+  measurementId: "G-7ZQYP3YZKS"
+};
 
-export { firestore };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);y
